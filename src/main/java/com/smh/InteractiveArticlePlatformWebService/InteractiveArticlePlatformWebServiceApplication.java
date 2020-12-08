b20c7195e95561b263290ab912c5373423042b2a
= <=>
2020-12-09 01:17:05 +0300

    Initial commit

---
+++ src/main/java/com/smh/InteractiveArticlePlatformWebService/InteractiveArticlePlatformWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smh.InteractiveArticlePlatformWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractiveArticlePlatformWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractiveArticlePlatformWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smh/InteractiveArticlePlatformWebService/InteractiveArticlePlatformWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smh.InteractiveArticlePlatformWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractiveArticlePlatformWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
